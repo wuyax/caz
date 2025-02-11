f3686dd458628205ef22f0d9e2a0c479d59426b6
wuyax <pengqingkai@gmail.com>
2025-02-11 14:18:32 +0800

    fix:version

--- Formula/caz.rb
+++ Formula/caz.rb
@@ -1,7 +1,7 @@
 class Caz < Formula
   desc "Clean .DS_Store files and create ZIP archives"
   homepage "https://github.com/wuyax/caz"
-  url "https://github.com/wuyax/caz/archive/v1.0.0.tar.gz"
+  url "https://github.com/wuyax/caz/archive/v1.1.1.tar.gz"
   sha256 "YOUR_TARBALL_SHA256" # 需要在发布后更新
   license "MIT"
 
